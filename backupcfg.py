d55509ceb6656491c48592b0df6ece17aff9d1f5
30023344 <109936929+30023344@users.noreply.github.com>
2022-09-13 17:04:32 +1000

    Add files via upload

--- backupcfg.py
+++ backupcfg.py
@@ -1,23 +1,26 @@
 
 writeToConsole = True
 interactive = False
+acceptMultibleArgs = True
 
 
 # to add more jobs, just copy one from below and change what is needed.
 # change filenames in quotes to whatever you want to back up. more files or folders can be added by sepreating them with commas.
-# complete paths also work.
+# Both relitave and absolute paths work.
 
-job1 = ["../README.md", "new text document.txt", "files go in here!/test7.py"]
-job2 = ["folder1"]
-job3 = ["new text document.txt"]
+job1 = ["../README.md", "./new text document.txt", "./files go in here!/test7.py"]
+job2 = ["./folder1"]
+job3 = ["./new text document.txt"]
 job4 = ["/home/ec2-user/environment/"]
+job5 = [".//"]
 #job0 = ["<filename>"] - dont forget file extensions!
 
 # old way of getting directory, now it supports different folders for different jobs
-backupDir = "backup" # could reference this in 'directory'
+backupDir = "./backup/" # could reference this in 'directory'
 
 # changing the bits in quotes changes what you type after 'backup.py' when you run it in console.
-jobs = {"job1": job1, "job2": job2, "job3": job3, "h": job4}
+jobs = {"job1": job1, "job2": job2, "job3": job3, "h": job4, "ohno": job5}
+
 # change the folder where backups get saved to by changing the bit after the job name.
-# both complete and relative paths work here.
-directory = {"job1": backupDir,  "job2": "h", "job3": "backup", "h": "backup"}
+# Both relitave and absolute paths work.
+directory = {"job1": backupDir,  "job2": "/home/ec2-user/environment/BackupScript 1.3.0/h", "job3": backupDir, "h": backupDir, "ohno": backupDir}
